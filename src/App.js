5b3c6d567f8e2c466ebd19f71e85e572edbf3107
ibrahim <megatey97@gmail.com>
2022-04-28 01:06:21 +0200

    latest new layout commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css'
+import Header from './components/header/Header';
 import HomePage from './components/HomePage/HomePage';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [feed, setFeed] = useState()
+  // const [title, setTitle] = useState()
+  let title;
+  const getFeed = async () => {
+      try {
+          const res = await axios.get('https://free-football-soccer-videos.p.rapidapi.com/', {
+            headers: {
+              'X-RapidAPI-Host': 'free-football-soccer-videos.p.rapidapi.com',
+              'X-RapidAPI-Key': 'cd70deb726msh5da57583305a4a0p138b03jsnfae7a783bd44'
+            }
+          })
+          const output = await res.data
+          console.log(output);
+          setFeed(output) 
+      } catch (err) {
+            console.log(err);
+      }
+  }
+
+useEffect(() => {
+  const getting = getFeed()
+
+  return () => {
+    getting()
+  }
+}, []) 
+
   return (
     <div className="App">
-     <HomePage/>
+      <Header/>
+     <HomePage feed={feed} title={title}/>
     </div>
   );
 }
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,136 @@
 import React from 'react'
+import './homepage.css'
 
-const HomePage = () => {
+const HomePage = ({feed, title}) => {
+    const date = new Date()
+    const isosDate = date.toISOString()
+    const spiltDate = isosDate.split('T')
+    const [fullDate, time] = spiltDate
+    console.log(spiltDate);
+    console.log(fullDate);
+
+    //latest Matches Selection
+    const allLatestMatch = feed?.filter(n => n.date.split('T')[0] === fullDate)
+    console.log(allLatestMatch?.slice(0, 10));
+    const latestMatches = allLatestMatch?.slice(0, 10)
+
+    //Premier League Matches Selection
+    const allPremierLeague = feed?.filter(n => n.competition.name === 'ENGLAND: Premier League')
+    const premierLeague = allPremierLeague?.slice(0, 10)
+    console.log(premierLeague);
+
+    //Laliga Matches Selection
+     const allLaliga = feed?.filter(n => n.competition.name === 'SPAIN: La Liga')
+    const laliga = allLaliga?.slice(0, 10)
+    console.log(laliga);
+
+    //Seria A Matches Selection
+    const allSeriaA = feed?.filter(n => n.competition.name === 'ITALY: Serie A')
+    const seriaA = allSeriaA?.slice(0, 10)
+    console.log(seriaA);
+
+    //Ligue 1 Matches Selection
+    const allLigue1 = feed?.filter(n => n.competition.name === 'FRANCE: Ligue 1')
+    const ligue1 = allLigue1?.slice(0, 10)
+    console.log(ligue1);
+
+    //Bundesliga Matches Selection
+    const allbundesliga = feed?.filter(n => n.competition.name === 'GERMANY: Bundesliga')
+    const bundesliga = allbundesliga?.slice(0, 10)
+    console.log(bundesliga);
+
+    title='name'
+    // console.log(title);
+
+
+
   return (
-    <div>HomePage</div>
+    <div>
+      {latestMatches && <div className="latest-matches-container">
+        <h2>Latest Matches</h2>
+        <div className="matches-list-container">
+          {
+            latestMatches?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+      {premierLeague && <div className="EPL-matches-container">
+        <h2>Premier League Matches</h2>
+        <div className="matches-list-container">
+          {
+            premierLeague?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+      {laliga && <div className="matches-container">
+        <h2>LaLiga Matches</h2>
+        <div className="matches-list-container">
+          {
+            laliga?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+      {seriaA && <div className="matches-container">
+        <h2>Seria A Matches</h2>
+        <div className="matches-list-container">
+          {
+            seriaA?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+      {ligue1 && <div className="matches-container">
+        <h2>Ligue 1 Matches</h2>
+        <div className="matches-list-container">
+          {
+            ligue1?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+      {bundesliga && <div className="matches-container">
+        <h2>Bundesliga Matches</h2>
+        <div className="matches-list-container">
+          {
+            bundesliga?.map(match => (<div key={match.indexOf} className='match-container'>
+              <div className="image-container">
+                <img src={match.thumbnail} alt="thumbnail" className='thumbnail' />
+              </div>
+              <p className="title">{match.title}</p>
+              <p className="date">{match.date}</p>
+            </div>))
+          }
+        </div>
+      </div>}
+    </div>
   )
 }
 
